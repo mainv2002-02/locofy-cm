3c5fdfabae53f39e5ae6f87774444cd447495dfb
Mai Nguyen <mainv2002@gmail.com>
2025-08-14 00:06:24 +0700

    update for frontend

---
+++ frontend/app/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// src/index.ts
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+console.log(greet("TypeScript"));
+//# sourceMappingURL=index.js.map
